29f7d4cbd9cc75fe7b9ff075ba0b0a50d5a8db01
afk <afk@ellugar.co>
2017-11-08 15:26:57 -0500

    Improved json output

--- src/containers/json/index.js
+++ src/containers/json/index.js
@@ -7,8 +7,21 @@ import './style.css';
 const mapStateToProps = state => {
   const {elements} = state || [];
 
+  const json = elements.map(item => {
+    delete item.pk;
+    delete item.elementType;
+    Object.keys(item).forEach(key => {
+      if (item[key] == null || item[key] === '') {
+        delete item[key];
+      } else if (key === 'linePoints') {
+        if (item[key].length < 1) delete item[key];
+      }
+    });
+    return item;
+  });
+
   const obj = {
-    components: elements,
+    components: json,
   };
 
   const items = JSON.stringify(obj);
@@ -22,6 +35,15 @@ let JsonPretty = ({json}) => (
   <div className="json">
     <JSONPretty
       json={json}
+      replacer={(key, value) => {
+        if (key === 'ok') {
+          value += '~~~abc';
+        }
+        if (key === 'gggg') {
+          value *= 10;
+        }
+        return value;
+      }}
       space="2"
     />
   </div>
