e5b454d10d11b6acf00352730cb0b3bd6587e684
afk <afk@ellugar.co>
2017-11-15 12:16:53 -0500

    Deleted old console log

--- src/containers/settings/index.js
+++ src/containers/settings/index.js
@@ -8,7 +8,6 @@ import './style.css';
 const mapStateToProps = state => {
   const {settings} = state || {columns: 0, rows: 0};
 
-  console.log(state);
   return {
     columns: settings.columns,
     rows: settings.rows,
